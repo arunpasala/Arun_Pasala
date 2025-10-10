519aa86b2d44345b739d476fd0d9e2b31ea46dc3
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2025-10-10 16:47:23 -0400

    Added jsx compents

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1 classNamey>Bala Arun Pasala</h1>
+
+        {/*Desktop view and navigation  */}
+        <nav>
+            <a href="">Home</a>
+            <a href="">Cerification</a>
+            <a href="">Projects</a>
+            <a href="">Services</a>
+            <a href="">Experience</a>
+        
+        </nav>
+        
+        <button className=''>Contact me</button>
+
+    </header>
+  )
+}
+
+export default Header
