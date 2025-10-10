51c7d583a5dd2cdfc52844676a09f3e2c4f48aa6
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2025-10-10 16:34:48 -0400

    Basic alignment on Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from 'react'
+
+import { useEffect } from "react";
+import Header from './components/Header';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Header/>
+    
+    </>
   )
 }
+
+export default App
