50261f13c1a6be59aae9cc9f091a6843b5474239
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-18 23:19:33 -0500

    working on book call

--- src/components/BookCall.jsx
+++ src/components/BookCall.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const BookCall = () => {
   return (
     <div>
-      
+      add useCallback(
+        () => {
+          callback
+        },
+        [input]
+      )
     </div>
   )
 }
